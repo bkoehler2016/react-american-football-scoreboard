aad736267d20d00100e49a557a404731d824491d
Ben Koehler <bkoehler85282@gmail.com>
2019-11-12 21:02:41 -0700

    fixed styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-//TODO: STEP 1 - Import the useState hook.
+// TODO: STEP 1 - Import the useState hook.
 import React, { useState } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
 function App() {
-  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  // TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, sethomeScore] = useState(0);
   const [awayScore, setawayScore] = useState(0);
   return (
